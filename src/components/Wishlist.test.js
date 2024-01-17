01e55038f7eb3888dab81df3eff6493a9df49e32
pcaracal <peter.schreivogel@stud.kbw.ch>
2024-01-17 11:52:27 +0100

    create template for wishlist test

---
+++ src/components/Wishlist.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Wishlist from "./Wishlist";
+
+jest.mock('./api', () => ({}));
+
+
+describe("Wishlist Component", () => {
+  test("renders Wishlist component", () => {
+  });
+
+  test("Display Wishes - should display all wishes without any filters", () => {
+  });
+
+  test("renders filtered month correctly", async () => {
+  });
+
+  function renderWishlist() {
+  }
+});
+
